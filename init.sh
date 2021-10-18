3f90dd52ac5a21e2eacfaad9ad3f8a08fadb7261
thirdbyte <59290972+thirdbyte@users.noreply.github.com>
2021-10-18 09:04:10 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ if [[ $EUID -ne 0 ]]; then
    echo "UID != 0" 
    exit 1
 fi
-packages="openssl unzip docker docker.io docker-compose net-tools iputils-ping iproute2 curl wget nano"
+packages="git openssl unzip docker docker.io docker-compose net-tools iputils-ping iproute2 curl wget nano"
 DEBIAN_FRONTEND=noninteractive apt-get -y update && apt-get -y dist-upgrade && apt-get install -y $packages && apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
 for image in rt:evilginx rt:gophish rt:nginx-proxy
 do
