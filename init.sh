4c2ec9fd18a1ee45b2c5bee7f1912c61ad55a75e
thirdbyte <59290972+thirdbyte@users.noreply.github.com>
2021-10-18 09:49:24 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ if [[ $EUID -ne 0 ]]; then
    echo "UID != 0" 
    exit 1
 fi
-packages="docker docker.io docker-compose curl wget nano git"
+packages="docker docker.io docker-compose"
 DEBIAN_FRONTEND=noninteractive apt-get -y update && apt-get -y dist-upgrade && apt-get install -y $packages && apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
 for image in evilginx gophish nginx-proxy
 do
