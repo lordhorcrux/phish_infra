afce2c2d9fedc660bd49db13ed9ab6f306902531
thirdbyte <59290972+thirdbyte@users.noreply.github.com>
2021-10-18 09:00:30 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,14 +3,13 @@ if [[ $EUID -ne 0 ]]; then
    echo "UID != 0" 
    exit 1
 fi
-dockerhub_username="scarfaced"
-phish_infra_dir=/opt/phish_infra
 packages="openssl unzip docker docker.io docker-compose net-tools iputils-ping iproute2 curl wget nano"
 DEBIAN_FRONTEND=noninteractive apt-get -y update && apt-get -y dist-upgrade && apt-get install -y $packages && apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
 for image in rt:evilginx rt:gophish rt:nginx-proxy
 do
-    docker pull $dockerhub_username/$image
-    docker tag $dockerhub_username/$image $image
-    docker rmi $dockerhub_username/$image
+    docker pull scarfaced/$image
+    docker tag scarfaced/$image $image
+    docker rmi scarfaced/$image
 done
-mkdir -p $phish_infra_dir/certs
+git clone https://github.com/thirdbyte/phish_infra /opt/phish_infra
+mkdir -p /opt/phish_infra
